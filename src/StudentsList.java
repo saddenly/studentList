36c162cdedfaabeb04153f4e19414137c71e8d21
Rustem <rustemandasov6@gmail.com>
2021-07-24 12:41:47 +0600

    initial

---
+++ src/StudentsList.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+public class StudentsList {
+    HashSet<String> studentsIds = new HashSet<>();
+
+    List<Student> studentsList = new ArrayList<>();
+
+    public void addStudent(Student student) {
+        if (!studentsIds.contains(student.getStudentId())) {
+            studentsList.add(student);
+        }
+        studentsIds.add(student.getStudentId());
+    }
+
+    public void printList() {
+        for (Student student : studentsList) {
+            System.out.println(student.toString());
+        }
+    }
+}
