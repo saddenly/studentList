839e7daacf4682d04f182a37d4cde67f45e08575
saddenly <rustemandasov6@gmail.com>
2021-11-28 14:41:19 +0600

    hashcode and equals

--- src/Student.java
+++ src/Student.java
@@ -37,4 +37,24 @@ public class Student {
     public String toString() {
         return name + ", " + group + ", " + studentId;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Student student = (Student) o;
+
+        if (name != null ? !name.equals(student.name) : student.name != null) return false;
+        if (group != null ? !group.equals(student.group) : student.group != null) return false;
+        return studentId != null ? studentId.equals(student.studentId) : student.studentId == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + (group != null ? group.hashCode() : 0);
+        result = 31 * result + (studentId != null ? studentId.hashCode() : 0);
+        return result;
+    }
 }
